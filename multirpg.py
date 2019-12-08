d2cdf44b5ee87b68bc29d92e1594a0a9bf2ade73
Dan Whitehouse <drw@dim-sum.org>
2019-12-08 13:00:35 +0000

    Melon Soda Update
    Added some pretty rad stuff.

--- multirpg.py
+++ multirpg.py
@@ -5,10 +5,6 @@ import re
 # register the script
 weechat.register("weechat-multirpg", "drwhitehouse", "1.0", "GPL3", "multirpg script", "", "")
 
-# some variables
-myserver="freenode"
-mynick="horseshoecrab"
-
 # callback for data received in input
 def buffer_input_cb(data, buffer, input_data):
     # ...
@@ -22,70 +18,106 @@ def buffer_close_cb(data, buffer):
 # print to buffer
 def displaybuffer(buffer, msg):
     weechat.prnt(buffer, msg)
+
+# send stats
+def sendstats(data,timer):
+    displaybuffer(scriptbuffer, "Sending stats:")
+    weechat.command(botbuffer, "stats")
     return weechat.WEECHAT_RC_OK
 
-# send query to bot
-def querybot(msg):
-    weechat.command(botbuffer, msg)
-    return weechat.WEECHAT_RC_OK
+# get seconds
+def getseconds(msg):
+    attackcounter = 0
+    displaybuffer(scriptbuffer, msg)
+    digits = [int(s) for s in re.findall(r'\b\d+\b', msg)]
+    for digit in digits:
+        attackcounter = attackcounter + digits[0] * 86400
+        attackcounter = attackcounter + digits[1] * 3600
+        attackcounter = attackcounter + digits[2] * 60
+        attackcounter = attackcounter + digits[3]
+    displaybuffer(scriptbuffer, str(attackcounter))
 
 #---------------------------------------------------------------------------#
 
 def msgparser(data, bufferp, tm, tags, display, is_hilight, prefix, msg):
-    displaybuffer(buffer, data)
-#    displaybuffer(buffer, bufferp)
-#    displaybuffer(buffer, tm)
-#    displaybuffer(buffer, tags)
-#   displaybuffer(buffer, display)      # Not strings
-#   displaybuffer(buffer, is_hilight)   # Don't prnt
-    displaybuffer(buffer, prefix)
-    displaybuffer(buffer, msg)
+    #displaybuffer(scriptbuffer, "---")
+    #displaybuffer(scriptbuffer, data)              # Very boring
+    #displaybuffer(scriptbuffer, bufferp)
+    #displaybuffer(scriptbuffer, tm)
+    #displaybuffer(scriptbuffer, tags)
+    #displaybuffer(scriptbuffer, str(display))      # Not strings
+    #displaybuffer(scriptbuffer, str(is_hilight))   # Don't prnt
+    #displaybuffer(scriptbuffer, prefix)
+    #displaybuffer(scriptbuffer, msg)
+    #displaybuffer(scriptbuffer, "---")
 
-    if msg.startswith("You can"):
+    if "You can now ATTACK." in msg:
+        displaybuffer(scriptbuffer, "Attacking:")
+        weechat.command(botbuffer, "attack lich")
+
+    if "You can now CHALLENGE." in msg:
+        displaybuffer(scriptbuffer, "Challenging:")
+        weechat.command(botbuffer, "challenge")
+
+    if msg.startswith("You can ATTACK"):
         chunks = msg.split("You can")
         for chunk in chunks:
-            displaybuffer(buffer, chunk)
-            if "ATTACK in" in chunk:
-                digits = [int(s) for s in re.findall(r'\b\d+\b', chunk)]
-                for digit in digits:
-                    displaybuffer(buffer, str(digit))
-                attackcounter = 0
-                attackcounter = attackcounter + digits[0] * 86400
-                attackcounter = attackcounter + digits[1] * 3600
-                attackcounter = attackcounter + digits[2] * 60
-                attackcounter = attackcounter + digits[3]
-                displaybuffer(buffer, str(attackcounter))
+	    if "in" in chunk:
+                getseconds(chunk)
+
     return weechat.WEECHAT_RC_OK
 
 #---------------------------------------------------------------------------#
 
 # create script buffer
-buffer = weechat.buffer_new("weechat-multirpg", "buffer_input_cb", "", "buffer_close_cb", "")
+scriptbuffer = weechat.buffer_new("weechat-multirpg", "buffer_input_cb", "", "buffer_close_cb", "")
 
 # set title
-weechat.buffer_set(buffer, "title", "weechat-multirpg - multirpg bot for weechat.")
+weechat.buffer_set(scriptbuffer, "title", "weechat-multirpg - multirpg bot for weechat.")
 
 # disable logging, by setting local variable "no_log" to "1"
-weechat.buffer_set(buffer, "localvar_set_no_log", "1")
-
-# create channel buffer
-chanbuffer = weechat.info_get("irc_buffer", "'{}', #multirpg".format(myserver))
+weechat.buffer_set(scriptbuffer, "localvar_set_no_log", "1")
 
 # start script
-displaybuffer(buffer, "Starting weechat-multirpg")
+displaybuffer(scriptbuffer, "Starting weechat-multirpg")
+displaybuffer(scriptbuffer, "")
+
+# create channel buffer
+chanbuffer = weechat.info_get("irc_buffer", "freenode, #multirpg")
 
 # query bot
 weechat.command(chanbuffer, "/query multirpg")
+botbuffer = weechat.current_buffer()
 
-# create bot buffer
-botbuffer = weechat.info_get("irc_buffer", "'{}', multirpg".format(myserver))
+# debug
+displaybuffer(scriptbuffer, "Buffer pointers:")
+displaybuffer(scriptbuffer, "")
+displaybuffer(scriptbuffer, "---")
+displaybuffer(scriptbuffer, "scriptbuffer:")
+displaybuffer(scriptbuffer,str(scriptbuffer))
+displaybuffer(scriptbuffer, "")
+displaybuffer(scriptbuffer, "chanbuffer:")
+displaybuffer(scriptbuffer,str(chanbuffer))
+displaybuffer(scriptbuffer, "")
+displaybuffer(scriptbuffer, "botbuffer:")
+displaybuffer(scriptbuffer,str(botbuffer))
+displaybuffer(scriptbuffer, "---")
 
-# timer test
-# weechat.hook_timer(60 * 1000, 60, 0, "querybot", "whoami")
+# hooks
+#weechat.hook_print(botbuffer, "", "", 0, "msgparser", "")
+#weechat.hook_print(chanbuffer, "nick_multirpg", "", 0, "msgparser", "")
+#weechat.hook_print("", "nick_multirpg", "", 0, "msgparser", "")
+#weechat.hook_print("", "notify_private,nick_multirpg", "", 0, "msgparser", "")
 
-# read test
-weechat.hook_print("'{}'", "", "'{}'", 0, "msgparser", "".format(chanbuffer, mynick))
-weechat.hook_print("'{}'", "notify_private", "", 1, "msgparser", "".format(botbuffer))
+# set timer
+weechat.hook_timer(75 * 60 * 1000, 0, 0, "sendstats", "")
 
-# getting stats
-querybot("stats")
+# set print hook
+weechat.hook_print("", "notify_private", "", 0, "msgparser", "") # Private only
+#weechat.hook_print("", "nick_multirpg", "", 0, "msgparser", "") # Multirpg
+
+# initialising
+displaybuffer(scriptbuffer, "Sending whoami & stats:")
+displaybuffer(scriptbuffer, "")
+weechat.command(botbuffer, "whoami")
+weechat.command(botbuffer, "stats")
